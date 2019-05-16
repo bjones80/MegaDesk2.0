eb3d5e7b7ccdfb5e65176a2ce2ebec6d6574dbab
sam-prettyman <35538856+sam-prettyman@users.noreply.github.com>
2019-05-16 10:32:12 -0600

    Changed display properties of ViewQuotes

--- MegaDesk-Jones/ViewQuotes.Designer.cs
+++ MegaDesk-Jones/ViewQuotes.Designer.cs
@@ -36,17 +36,19 @@
             // 
             this.view.AutoSize = true;
             this.view.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.view.Location = new System.Drawing.Point(263, 40);
+            this.view.Location = new System.Drawing.Point(351, 49);
+            this.view.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.view.Name = "view";
-            this.view.Size = new System.Drawing.Size(258, 46);
+            this.view.Size = new System.Drawing.Size(319, 58);
             this.view.TabIndex = 0;
             this.view.Text = "View Quotes";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(360, 192);
+            this.button1.Location = new System.Drawing.Point(485, 501);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 1;
             this.button1.Text = "Cancle";
             this.button1.UseVisualStyleBackColor = true;
@@ -54,11 +56,12 @@
             // 
             // ViewQuotes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.view);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ViewQuotes";
             this.Text = "View Quotes";
             this.ResumeLayout(false);
